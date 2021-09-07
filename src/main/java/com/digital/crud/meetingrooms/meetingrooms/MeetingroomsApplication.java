f86d8c4426f4987d0305aa4f8b2b4f6b6a64ff70
kyo1506 <vinicius_pinheiro02@hotmail.com>
2021-09-06 21:19:38 -0300

    Initial commit

---
+++ src/main/java/com/digital/crud/meetingrooms/meetingrooms/MeetingroomsApplication.java
@@ -0,0 +1,13 @@
+package com.digital.crud.meetingrooms.meetingrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/crud/meetingrooms/meetingrooms/MeetingroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.crud.meetingrooms.meetingrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
